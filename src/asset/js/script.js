6c546e2fc92ed8b090d1b6252f0978f1cc5b8e93
kvu-luong <luongkhanhvu1392@gmail.com>
2019-01-21 21:07:55 +0700

    little sp

--- src/asset/js/script.js
+++ src/asset/js/script.js
@@ -1,28 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     setting_slick(".js-header-info__slider");
     setting_slick(".js-sweet__slider");
 
-        $('#js-menu-icon').click(function(){
-            $(this).toggleClass('open');
-            
-            if($( "#js-menu-icon" ).hasClass( "open")){
-                $(".js-navbar-sp").css("display","block");
-            }else{
-                $(".js-navbar-sp").css("display","none");
-            }
-        });
-       
-   
+    var browser_w = $(window).width();
+    console.log(browser_w);
+    if (browser_w < 768) {
+        $(".header-sp").css("display", "block");
+    } else {
+        $(".header-sp").css("display", "none");
+    }
+    $('#js-menu-icon').click(function () {
+        $(this).toggleClass('open');
+
+        if ($("#js-menu-icon").hasClass("open")) {
+            $(".js-navbar-sp").css("display", "flex");
+            $("  #js-menu-icon").css("margin", "0 30px 0 0");
+        } else {
+            $(".js-navbar-sp").css("display", "none");
+            $("  #js-menu-icon").css("margin", "30px 30px 0 0");
+
+        }
+    });
+
+
 });
-function setting_slick(class_name){
+
+function setting_slick(class_name) {
     $(class_name).slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         infinite: false,
         autoplay: true,
         autoplaySpeed: 8000,
-        arrow:false,
+        arrow: false,
         fade: true,
         dots: false,
         prevArrow: false,
